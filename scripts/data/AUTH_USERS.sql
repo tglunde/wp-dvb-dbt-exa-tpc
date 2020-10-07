9595ed1385671b08e5e8277858fb67a0a1db168f
tglunde <torsten@glunde.de>
2020-10-07 16:31:47 +0200

    removed double export

--- scripts/data/AUTH_USERS.sql
+++ scripts/data/AUTH_USERS.sql
@@ -1,2 +1,1 @@
 1,dvb,your@mail.com,"Added via Docker parameters",$2a$08$0veurFZbR/x0AqVfvF9yEeDieG1bIs4LdfcsWy/RIOKexk8gHcRiq,,dvb_admin,0,0
-1,dvb,your@mail.com,"Added via Docker parameters",$2a$08$FEPGAIwJblrzrUisP9BTouCFkSu2Te30vr1y8PC4PzxzeJS0StILi,,dvb_admin,0,0
