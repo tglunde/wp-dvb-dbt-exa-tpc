3ed524a4a4a62051a284ceb280b315666363040f
ilikutle <ilijakutle@gmail.com>
2020-10-12 13:54:36 +0000

    changed import scripts

--- scripts/exa_import.sh
+++ scripts/exa_import.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 EXA_PORT=$(docker port exa_exasol_1 | grep 'tcp' | grep '8888' | grep -oP '(?<=:)\w+')
-DSN=localhost:$EXA_PORT
+#DSN=localhost:$EXA_PORT
+DSN=52.143.0.192:8563
 USER=sys
-PWD=exasol
+PWD=Start123
 EXAPLUS=exaplus
 
 $EXAPLUS -c $DSN -u $USER -p $PWD -sql 'create schema if not exists EXA_TOOLBOX;'
--- scripts/import_data.sh
+++ scripts/import_data.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+#EXA_PORT=$(docker port exa_exasol_1 | grep 'tcp' | grep '8888' | grep -oP '(?<=:)\w+')
+#DSN=localhost:$EXA_PORT
+
+DSN=52.143.0.192:8563
+USER=sys
+PWD=Start123
+EXAPLUS=exaplus
+IMPORT_FILE=./import_sf500.sql
 
 #Create schema and tables
-/datadrive/EXAplus-6.2.3/exaplus -c localhost:8888 -u sys -p exasol -f ./sql_dump.sql 
+$EXAPLUS -c $DSN -u $USER -p $PWD -f ./sql_dump.sql
 
 #Import data 
-/datadrive/EXAplus-6.2.3/exaplus -c localhost:8888 -u sys -p exasol -s dbt -f ./import.sql
-
-
+ls | parallel -j 8 -a $IMPORT_FILE $EXAPLUS - c $DSN -u $USER -p $PWD -s dbt -sql
