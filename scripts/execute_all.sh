7dd1ad44c7f4175602b2a22770e0117ccb8b2325
Torsten Glunde <glunde-extern@gvl.de>
2020-12-18 18:07:06 +0100

    planned task exasol+snowflake added

--- scripts/execute_all.sh
+++ scripts/execute_all.sh
@@ -1,6 +1,4 @@
-python3 ~/wp-dvb-dbt-exa-tpc/scripts/job_exec.py
+time python3 ~/wp-dvb-dbt-exa-tpc/scripts/job_exec.py
 
 cd ~/wp-dvb-dbt-exa-tpc/dbt
-dbt run --profiles-dir .
-
-
+time dbt run --profiles-dir .
